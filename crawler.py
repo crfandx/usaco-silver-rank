7ff5e7c73fcb3829ab413d0dca1be330063abc8c
crfandx <116484718+crfandx@users.noreply.github.com>
2026-06-07 12:27:30 +0000

    style: auto-format code [skip ci]

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
-import requests
 import logging
 from time import sleep
+
+import requests
 from bs4 import BeautifulSoup
 
 header = {
@@ -11,21 +12,54 @@ header = {
 # Custom settings
 PAGES = range(10, 15)
 PROBLEM_IDS = list(range(1201, 1427))
-USER_UIDS = [653, 339, 474, 494, 488, 216, 465, 478, 
-            482, 481, 338, 333, 479, 335, 486, 331, 
-            329, 307, 340, 336, 208, 222, 487, 495, 
-            214, 468, 470, 197, 167, 469, 282, 143, 177, 189]
+USER_UIDS = [
+    653,
+    339,
+    474,
+    494,
+    488,
+    216,
+    465,
+    478,
+    482,
+    481,
+    338,
+    333,
+    479,
+    335,
+    486,
+    331,
+    329,
+    307,
+    340,
+    336,
+    208,
+    222,
+    487,
+    495,
+    214,
+    468,
+    470,
+    197,
+    167,
+    469,
+    282,
+    143,
+    177,
+    189,
+]
 
 USER_NUM = len(USER_UIDS)
 PROBLEM_NUM = len(PROBLEM_IDS)
 problem_names = []
 users = []
 
+
 def get_problems(file):
     for page in PAGES:
         url = f"http://oi.bashu.cn/d/junior/p?page={page}"
         response = requests.get(url, headers=header)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
         problems = soup.select("td.col--problem-name")
         for problem in problems:
             id_tag = problem.select_one("b")
@@ -45,7 +79,9 @@ def get_problems(file):
         sleep(0.1)
 
     if len(problem_names) != PROBLEM_NUM:
-        logging.warning(f"The number of problems from OJ is different from the number of problems from input. {len(problem_names)}/{PROBLEM_NUM}")
+        logging.warning(
+            f"The number of problems from OJ is different from the number of problems from input. {len(problem_names)}/{PROBLEM_NUM}"
+        )
 
     buf = ""
     for id in PROBLEM_IDS:
@@ -58,12 +94,13 @@ def get_problems(file):
     buf += "\n"
     file.write(buf)
 
+
 def get_user_status(file):
     order = 0
     for uid in USER_UIDS:
         url = f"http://oi.bashu.cn/d/junior/user/{uid}#tab-1"
         response = requests.get(url, headers=header)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
         username_tag = soup.select_one("h1")
         if username_tag is None:
             logging.error("username_tag is None.")
@@ -74,11 +111,11 @@ def get_user_status(file):
         if username == "登录":
             logging.error("Fail to log in.")
             raise RuntimeError("Fail to log in.")
-        
+
         status = [0] * PROBLEM_NUM
         AC_problem_ids = soup.select("a")
         for id in AC_problem_ids:
-            if(len(id.contents) == 0):
+            if len(id.contents) == 0:
                 continue
             if len(str(id.contents[0]).split(">")) >= 2:
                 AC_problem_id = str(id.contents[0]).strip("<b>").strip("</b>")
@@ -88,16 +125,20 @@ def get_user_status(file):
                 if AC_problem_id in PROBLEM_IDS:
                     index = PROBLEM_IDS.index(AC_problem_id)
                     status[index] = 1
-                    logging.info(f"User {username} has AC a problem. id: {AC_problem_id}")
+                    logging.info(
+                        f"User {username} has AC a problem. id: {AC_problem_id}"
+                    )
         AC_problem_num = status.count(1)
         order += 1
-        logging.info(f"User {username} has AC {AC_problem_num} problem(s). {order}/{USER_NUM}")
+        logging.info(
+            f"User {username} has AC {AC_problem_num} problem(s). {order}/{USER_NUM}"
+        )
         global users
         user = [0, username, AC_problem_num] + status.copy()
         users.append(user)
 
         sleep(0.1)
-    
+
     users = sorted(users, key=lambda x: -x[2])
     for rank in range(len(users)):
         if rank > 0 and users[rank][2] == users[rank - 1][2]:
@@ -112,16 +153,18 @@ def get_user_status(file):
         buf += "\n"
         file.write(buf)
 
+
 import os
 
+
 def main():
-    sid = os.environ.get('SID')
-    sid_sig = os.environ.get('SID_SIG')
-    
+    sid = os.environ.get("SID")
+    sid_sig = os.environ.get("SID_SIG")
+
     if not sid or not sid_sig:
         logging.error("Missing SID or SID_SIG environment variables.")
         raise ValueError("Missing SID or SID_SIG environment variables.")
-    
+
     header["Cookie"] = f"sid={sid}; sid.sig={sid_sig}"
     logging.basicConfig(level=logging.INFO)
     logging.info("Begin to crawl the problems and the statuses.")
@@ -131,6 +174,7 @@ def main():
         logging.info("All the problems have been crawled.")
         get_user_status(file)
         logging.info("All the statuses have been crawled.")
+
 
 if __name__ == "__main__":
     main()
